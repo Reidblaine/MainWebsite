112fc21047c1f42b6c3c5b4c90450e13755bdfc1
Reid Blaine <reidblaine@Reids-MacBook-Air.local>
2025-04-23 14:24:53 +0100

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { Providers } from '@/components/Providers'
 import './globals.css'
 import CursorTrail from '@/components/CursorTrail'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Providers>
           <CursorTrail />
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
