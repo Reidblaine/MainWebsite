3b19bd9711361e67d7b6629cded4f28e48645522
Reid Blaine <reidblaine@Reids-MacBook-Air.local>
2025-04-23 14:38:32 +0100

    feat: add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Providers } from '@/components/Providers'
 import './globals.css'
 import CursorTrail from '@/components/CursorTrail'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <CursorTrail />
           {children}
           <Analytics />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
